c4cb93f9d11867b40996eba35f96a265442418bd
matheusp94 <matheus.p940@gmail.com>
2023-09-25 19:33:59 -0300

    aprendendohooks

---
+++ app/components/PrintMessage/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PrintMessage = ()=>{
+    return <h1>Meu nome é</h1>
+}
+
+export default PrintMessage
--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import PrintMessage from './components/PrintMessage';
 
 export default function Home() {
   return (
     <main>
-      
+      <PrintMessage />
     </main>
     
   )
