3e1b34f6f471f84a08ba07e0edf22c7e465a025d
matheusp94 <matheus.p940@gmail.com>
2023-09-25 19:47:23 -0300

    correção 2

--- app/components/PrintMessage/index.js
+++ app/components/PrintMessage/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PrintMessage = (props) => {
-    return <h1>Meu nome é{props.name}</h1>
+    return <h1>Meu nome é {props.name}</h1>
 }
 
 export default PrintMessage
--- app/page.js
+++ app/page.js
@@ -1,13 +1,16 @@
+'use client';
+
 import PrintMessage from './components/PrintMessage';
-
-
+import { useState } from 'react';
 
 export default function Home() {
+  const state = useState();
+  console.log('State = ', state)
+
   const name = "FIAP";
   return (
     <main>
       <PrintMessage name={name}/>
-    </main>
-    
+    </main> 
   )
 }
